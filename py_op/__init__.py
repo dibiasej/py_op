4002c7cd708dc5eb1ad03c4164591a2da2f813f2
dibiasej <dibiasej3@gmail.com>
2026-03-13 17:46:13 -0400

    added rolling skew

--- py_op/__init__.py
+++ py_op/__init__.py
@@ -24,6 +24,7 @@ from py_op.data.price_data import process_price_data
 from py_op.data.builders.option_chain_builder import create_chain, create_chain_series
 
 # for analytics I am thinking I want it to look like py_op.analytics.RollingTermStructure() in a notebook
+from py_op.analysis.rolling_analytics.implied_surface import RollingSkew # we will change this in the future
 
 __all__ = [
     # Pricing modules
@@ -42,7 +43,9 @@ __all__ = [
     # Data
     "process_price_data",
     "create_chain", 
-    "create_chain_series"
+    "create_chain_series",
+    # analytics
+    "RollingSkew"
 ]
 
 __version__ = "0.1.0"
--- py_op/analysis/rolling_analytics/implied_surface.py
+++ py_op/analysis/rolling_analytics/implied_surface.py
@@ -1,5 +1,7 @@
+import numpy as np
 
 from py_op.data.builders.option_chain_builder import create_chain_series
+from py_op.calc_engine.vol_engine.iv_calc import RootFinder
 """
 (3/1/2026) We will move the skew and constant maturity IV's from implied_values.py into here
 """
@@ -30,7 +32,76 @@ class RollingSkew:
         self.end_date = end_date
         self.steps = steps
         self.chain_series = create_chain_series(ticker, start_date, end_date, steps=steps)
+
+    def implied_skew_moneyness(self, target_dte: int, moneyness: float=0.1, max_days_diff: int=20):
+        """
+        This is the version of implied skew from Euan Sinclair retail option trading. It is also number 6 in the flash cards ipynb
+        The issue with this is we need to make it constant maturity right now it just fetches the maturity closest to dte
+        Note: (2/16/26) we will change this so it gets a constant maturity dte not the closest
+        """
+        implied_skews, dates = [], []
+        rf = RootFinder()
+
+        for chain in self.chain_series:
+            S = chain.S
+
+            otm_prices, strikes, actual_dte = chain.get_otm_skew_prices(
+                dte=target_dte, max_days_diff=max_days_diff
+            )
+
+            strikes = np.array(strikes, dtype=float)
+            m_arr = strikes / S
+
+            idx_put  = np.abs(m_arr - (1 - moneyness)).argmin()
+            idx_call = np.abs(m_arr - (1 + moneyness)).argmin()
+
+            put_price,  K_put  = float(otm_prices[idx_put]),  float(strikes[idx_put])
+            call_price, K_call = float(otm_prices[idx_call]), float(strikes[idx_call])
+
+            T = actual_dte / 365.0  # use the returned DTE for pricing
+
+            put_iv  = rf.calculate(put_price,  S, K_put,  T, otype="put")
+            call_iv = rf.calculate(call_price, S, K_call, T, otype="call")
+
+            denom = (K_put - K_call) / S
+            implied_skews.append((put_iv - call_iv) / denom)
+            dates.append(chain.close_date)
+
+        return implied_skews, dates
     
+    def implied_skew_fixed_strike(self, target_dte: int, moneyness: float = 0.1, max_days_diff: int = 20):
+        """
+        This measures 90% put iv - 110% call iv -- we should make a modification that allows 100% atm iv.
+        Collin Bennet refers to 90% put iv - 100% atm iv as fixed strike skew.
+        """
+        implied_skews, dates = [], []
+        rf = RootFinder()
+
+        for chain in self.chain_series:
+            S = chain.S
+
+            otm_prices, strikes, actual_dte = chain.get_otm_skew_prices(
+                dte=target_dte, max_days_diff=max_days_diff
+            )
+
+            strikes = np.array(strikes, dtype=float)
+            m_arr = strikes / S
+
+            idx_put  = np.abs(m_arr - (1 - moneyness)).argmin()
+            idx_call = np.abs(m_arr - (1 + moneyness)).argmin()
+
+            put_price,  K_put  = float(otm_prices[idx_put]),  float(strikes[idx_put])
+            call_price, K_call = float(otm_prices[idx_call]), float(strikes[idx_call])
+
+            T = actual_dte / 365.0  # use the returned DTE for pricing
+
+            put_iv  = rf.calculate(put_price,  S, K_put,  T, otype="put")
+            call_iv = rf.calculate(call_price, S, K_call, T, otype="call")
+
+            implied_skews.append(put_iv - call_iv)
+            dates.append(chain.close_date)
+
+        return implied_skews, dates
 
 class RollingKurtosis:
     pass
